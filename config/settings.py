36d4927fb9c7a2c6676c0a50a75f2562d30a2b53
shuo-jia <shuojialiu@outlook.com>
2026-06-22 18:30:59 +0800

    移除数据库配置文件路径的硬编码

--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': './stock.cnf',
+            'read_default_file': str(BASE_DIR / 'stock.cnf'),
         },
     }
 }
